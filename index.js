7146753d9e7d8ffc29cd239f0499e48fd85d5150
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-10-03 12:00:51 +0200

    Done

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 'use strict';
 
 console.log('funguju!');
+
+const timeInput = document.querySelector('.time-input');
+let time = '';
+const formElement = document.querySelector('form');
+const audioElement = document.querySelector('audio');
+let alarmSecondsElement = document.querySelector('.alarm__seconds');
+let alarmMinutesElement = document.querySelector('.alarm__minutes');
+
+const countDown = (e) => {
+  e.preventDefault();
+  time = Number(timeInput.value);
+
+  const timerId = setInterval(() => {
+    if (time === 0) {
+      clearInterval(timerId);
+      audioElement.play();
+    } else {
+      time -= 1;
+      alarmMinutesElement.textContent = `${String(
+        Math.floor(time / 60),
+      ).padStart(2, '0')}`;
+      alarmSecondsElement.textContent = `${String(
+        Math.round(time % 60),
+      ).padStart(2, '0')}`;
+    }
+  }, 1000);
+};
+formElement.addEventListener('submit', countDown);
