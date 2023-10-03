81b193bf533a14ea1716e3ccaaebe7baabad6b05
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-10-03 12:06:51 +0200

    úprava kodu,komentáře

--- index.js
+++ index.js
@@ -3,29 +3,35 @@
 console.log('funguju!');
 
 const timeInput = document.querySelector('.time-input');
-let time = '';
 const formElement = document.querySelector('form');
 const audioElement = document.querySelector('audio');
 let alarmSecondsElement = document.querySelector('.alarm__seconds');
 let alarmMinutesElement = document.querySelector('.alarm__minutes');
+let time = '';
 
 const countDown = (e) => {
   e.preventDefault();
-  time = Number(timeInput.value);
+  time = Number(timeInput.value); //zadání hodnoty time od U
 
+  //převedení setIntervalu na const, aby to šlo poté vypnout
   const timerId = setInterval(() => {
+    //Vypršení odpočetu = zapnutí budíku a vypnutí setInterval
     if (time === 0) {
       clearInterval(timerId);
       audioElement.play();
+      //Odečítání po 1s a vypsání zbývajícího času do budíku
     } else {
       time -= 1;
+      //Zadání minut
       alarmMinutesElement.textContent = `${String(
         Math.floor(time / 60),
       ).padStart(2, '0')}`;
+      //Zadání sekund
       alarmSecondsElement.textContent = `${String(
         Math.round(time % 60),
       ).padStart(2, '0')}`;
     }
   }, 1000);
 };
+
 formElement.addEventListener('submit', countDown);
